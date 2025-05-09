6f26b39b6da56cc2bcb98c452717e1f541a5d984
davidgo24 <113555992+davidgo24@users.noreply.github.com>
2025-05-08 18:41:48 -0700

    finalized the data analysis, and formatting for the project

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from stats import whats_the_word
+import sys
+from stats import whats_the_word, character_counter, pretty
+
+if len(sys.argv) < 2:
+	print("Usage: python3 main.py <path_to_book>")
+	sys.exit(1)
+
+filepath = sys.argv[1]
 
 def get_book_text(filepath):
 	with open(filepath) as f:
@@ -6,8 +13,16 @@ def get_book_text(filepath):
 	return file_contents
 
 def main():
-	file_contents = get_book_text("./books/frankenstein.txt")
+	file_contents = get_book_text(filepath)
+	print("============ BOOKBOT ============")
+	print(f"Analyzing book found at {filepath}...")
+
 	numw = whats_the_word(file_contents)
-	print(f"{numw} words found in the document")
+	print("----------- Word Count ----------")
+	print(f"Found {numw} total words")
+	final_counts = character_counter(file_contents)
+	print("--------- Character Count -------")
+	print(pretty(final_counts))
+	print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,47 @@ def whats_the_word(file_contents):
 	iso_words_list = file_contents.split()
 	word_count = len(iso_words_list)
 	return word_count
+
+
+
+def character_counter(text):
+	iso_characters = list(text.lower())
+	dictionary = {}
+
+	for char in iso_characters:
+		if char.isalpha():
+			if char in dictionary:
+				dictionary[char] += 1
+			else:
+				dictionary[char] = 1
+	return dictionary
+
+	
+
+
+
+def pretty(counts):
+	## we want to return a sorted list of dictionaries such as..
+	## each dictionary should have a key value pair as such {"char": "d", "num": 24}
+	## sort the list from greates to least count
+	## use the sort() method here as fit 
+	
+	#1. i want to assign the first key in the counts dictionary we are accepting to the value
+	#    of the first key we are creating in a pre-defined dictionary ^
+	
+	list_pairs = []
+
+	for key in counts:
+		pair = {}
+		pair["char"] = key
+		pair["num"] = counts[key]
+		list_pairs.append(pair)
+	
+	list_pairs.sort(key=lambda d: d["num"], reverse=True)
+	
+	output = ""
+	
+	for pair in list_pairs:
+		output += f"{pair['char']}: {pair['num']}\n"
+	return output.strip()
+			
